aa0551920d82fa6c4a38e7e144548285ffc471ef
LNQB-Dev <baodongthanh1994@gmail.com>
2026-03-14 15:05:47 +0700

    jsdh

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fec74370760c53ec4eb6a58c250409c394baefa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dcd5a9997c581b33ccc6050fac496bbf32f4da3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
