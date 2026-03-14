93aae634fac2c4ba74ac7b95783aee53f1ff1ec5
LNQB-Dev <baodongthanh1994@gmail.com>
2026-03-14 15:10:41 +0700

    haha

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dcd5a9997c581b33ccc6050fac496bbf32f4da3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0551920d82fa6c4a38e7e144548285ffc471ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
